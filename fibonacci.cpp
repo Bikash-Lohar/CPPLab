8a90aca97a16a526034285e11e2b833a8d0ce0fa
Bikash-Lohar <140369054+Bikash-Lohar@users.noreply.github.com>
2023-07-27 11:12:59 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -10,6 +10,7 @@ int main (){
 }
 void fibonacci(int n){
     int term1 = 0,term2 = 1,sum;
+    cout<<"Fibonacci series upto term:"<<n<<endl;
     if ( n == 0){
         cout<<"Enter atleast 1 term!\n";
         return;
@@ -22,7 +23,6 @@ void fibonacci(int n){
         cout<<term1<<"\t"<<term2<<"\t";
         return;
     }
-    cout<<"Fibonacci series upto term:"<<n<<endl;
     cout<<term1<<"\t"<<term2<<"\t";
     for (int i = 2; i<n; i++){
         sum = term1 + term2;
