ef97495eb3ad436cc455d6b66d96612f4950211b
Bikash Lohar <140369054+Bikash-Lohar@users.noreply.github.com>
2023-08-12 08:12:33 +0530

    Update primeproduct.cpp
    this is the correct version of the problem

--- primeproduct.cpp
+++ primeproduct.cpp
@@ -3,20 +3,20 @@ using namespace std;
 int prime (int n);
 //can i return an array from a function ans no!
 int main () {
-    int num;
+    int num,ans;
     cout<<"Enter the term:";
     cin>>num;
-    cout<<"The answer is "<<prime(num);
+    ans = prime(num);
+    cout<<"The product is "<<ans;
     return 0;
 }
 int prime (int n){
     int i,j,product = 1,flag = 0;
     if (n <= 1){
-        cout<<"The product is 0";
         return 0;
     }
     cout<<"1 is not a prime number\n";
-    for (i = 2; i<=; i++){
+    for (i = 2; i<=n; i++){
         flag = 0;
         for (j = 2; j<i; j++){
             if (i%j == 0){
